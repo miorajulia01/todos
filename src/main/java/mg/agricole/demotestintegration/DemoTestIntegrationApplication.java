f77866981e5c50e474cc645c8d08fd6e9b511af6
miorajulia01 <miorahanitra01.com>
2026-07-20 11:18:38 +0300

    chore: initialize project configuration

---
+++ src/main/java/mg/agricole/demotestintegration/DemoTestIntegrationApplication.java
@@ -0,0 +1,13 @@
+package mg.agricole.demotestintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTestIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTestIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mg/agricole/demotestintegration/DemoTestIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.agricole.demotestintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
